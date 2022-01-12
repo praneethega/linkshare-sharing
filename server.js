4f131d79b3b18a610bfceb1d1719c170e2e23305
praneethega <praneethega333@gmail.com>
2022-01-12 14:58:38 +0530

    'addingcors'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-/*const cors = require('cors');*/
+const cors = require('cors');
 
 const PORT =process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -9,11 +9,12 @@ app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
 
-//cors
-/*const corsOptions ={
-    origin: process.env.ALLOWED_CLIENTS.split(',')
+//Cors
+const corsOptions ={
+    origin: ['http://localhost:3000','http://localhost:5000']
 }
-app.use(cors(corsOptions));*/
+
+app.use(cors(corsOptions));
 
 //template engine
 app.set('views',path.join(__dirname, '/views')); 
